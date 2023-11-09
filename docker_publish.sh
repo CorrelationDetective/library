eeb88e19c03005d04cca10ac3f1e62d413e5514b
Jens d'Hondt <45044727+JdHondt@users.noreply.github.com>
2023-11-09 16:12:26 +0100

    Delete docker_publish.sh

--- docker_publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mvn clean package -DskipTests
-
-docker login
-
-docker build -t correlationdetective/correlationdetective:latest .
-
-docker push correlationdetective/correlationdetective:latest
